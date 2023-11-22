3968762ccbe603bb50ecc0ff22c3c954ae7c37ed
TalissonCdR <talissonrosa.pl008@academico.ifsul.edu.br>
2023-11-22 17:34:43 -0300

    finalização

--- src/main/java/br/edu/ifsul/cstsi/lpoo_objetivo_5/model/Leilao.java
+++ src/main/java/br/edu/ifsul/cstsi/lpoo_objetivo_5/model/Leilao.java
@@ -24,4 +24,68 @@ public class Leilao {
 
    @OneToMany(mappedBy = "leilao")
     private List<Item_Leilao> item_leilao;
+
+    public Leilao() {
+    }
+
+    public Leilao(LocalDate dt_inicio, LocalTime hor_inicio, LocalDate dt_final, LocalTime hor_final, List<Item_Leilao> item_leilao) {
+        this.dt_inicio = dt_inicio;
+        this.hor_inicio = hor_inicio;
+        this.dt_final = dt_final;
+        this.hor_final = hor_final;
+        this.item_leilao = item_leilao;
+    }
+
+    public LocalDate getDt_inicio() {
+        return dt_inicio;
+    }
+
+    public void setDt_inicio(LocalDate dt_inicio) {
+        this.dt_inicio = dt_inicio;
+    }
+
+    public LocalTime getHor_inicio() {
+        return hor_inicio;
+    }
+
+    public void setHor_inicio(LocalTime hor_inicio) {
+        this.hor_inicio = hor_inicio;
+    }
+
+    public LocalDate getDt_final() {
+        return dt_final;
+    }
+
+    public void setDt_final(LocalDate dt_final) {
+        this.dt_final = dt_final;
+    }
+
+    public LocalTime getHor_final() {
+        return hor_final;
+    }
+
+    public void setHor_final(LocalTime hor_final) {
+        this.hor_final = hor_final;
+    }
+
+    public List<Item_Leilao> getItem_leilao() {
+        return item_leilao;
+    }
+
+    public void setItem_leilao(List<Item_Leilao> item_leilao) {
+        this.item_leilao = item_leilao;
+    }
+
+
+    @Override
+    public String toString() {
+        return "\nLeilao{" +
+                "id=" + id +
+                ", dt_inicio=" + dt_inicio +
+                ", hor_inicio=" + hor_inicio +
+                ", dt_final=" + dt_final +
+                ", hor_final=" + hor_final +
+                ", item_leilao=" + item_leilao +
+                '}';
+    }
 }
--- src/main/java/br/edu/ifsul/cstsi/lpoo_objetivo_5/model/Participante.java
+++ src/main/java/br/edu/ifsul/cstsi/lpoo_objetivo_5/model/Participante.java
@@ -28,4 +28,88 @@ public class Participante {
 
     @OneToMany(mappedBy = "participante")
     private List<Lance> lances;
+
+    public Participante() {
+    }
+
+    public Participante(String nome, String login, String senha, String email, String endereco, String telefone, List<Lance> lances) {
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        this.email = email;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.lances = lances;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public List<Lance> getLances() {
+        return lances;
+    }
+
+    public void setLances(List<Lance> lances) {
+        this.lances = lances;
+    }
+
+
+    @Override
+    public String toString() {
+        return "\nParticipante{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", login='" + login + '\'' +
+                ", senha='" + senha + '\'' +
+                ", email='" + email + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", lances=" + lances +
+                '}';
+    }
 }
